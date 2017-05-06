c47bc445190a07ba2ff286410cde6957d3537ade
if15b065 <if15b065@technikum-wien.at>
2017-05-06 17:12:28 +0200

    headless browser unit tests

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -100,7 +100,8 @@ module.exports = function (config) {
      * available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
      */
     browsers: [
-      'Chrome'
+      //'Chrome'
+	  'PhantomJS'
     ],
 
     customLaunchers: {
